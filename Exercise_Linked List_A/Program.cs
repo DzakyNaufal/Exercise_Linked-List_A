0b4639233d9dfb2fcacdd86b0b33a49695bcd682
DzakyNaufal <dzakynmulna@gmail.com>
2022-11-23 15:41:24 +0700

    membuat class CircularList

--- Exercise_Linked List_A/Program.cs
+++ Exercise_Linked List_A/Program.cs
@@ -8,9 +8,18 @@ namespace Exercise_Linked_List_A
 {
     class Node
     {
-        //creates Nodes for the circular nexted list//
+        /*creates Nodes for the circular nexted list*/
         public int rollNumber;
         public string name;
         public Node next;
+    }
+    class CircularList
+    {
+        Node LAST;
+
+        public CircularList()
+        {
+            LAST = null;
+        }
     }
 }
