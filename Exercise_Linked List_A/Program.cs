ee4fda70870067c8ef5f1d12cb5160d4f600f860
DzakyNaufal <dzakynmulna@gmail.com>
2022-11-23 16:02:53 +0700

    membuat list semua node pada traverse

--- Exercise_Linked List_A/Program.cs
+++ Exercise_Linked List_A/Program.cs
@@ -49,7 +49,16 @@ namespace Exercise_Linked_List_A
                 Console.WriteLine("\nList is Empty");
             else
             {
-
+                Console.WriteLine("\nRecords in the list are:\n");
+                Node currentNode;
+                currentNode = LAST.next;
+                while (currentNode != LAST)
+                {
+                    Console.Write(currentNode.rollNumber + "   " +
+                        currentNode.name + "\n");
+                    currentNode = currentNode.next;
+                }
+                Console.Write(LAST.rollNumber + "   " + LAST.name + "\n");
             }
 
         }
